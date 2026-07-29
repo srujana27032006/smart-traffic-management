290af60f99af9769b8fd4712109e7c0c88296bf7
srujana27032006 <srujana27032006@users.noreply.github.com>
2026-07-29 11:28:22 +0530

    Fix router 404 on GitHub Pages using wouter hash routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -9,13 +10,16 @@ import Dashboard from "@/pages/Dashboard";
 
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter hook={useHashLocation}>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
+
 
 function App() {
   return (
